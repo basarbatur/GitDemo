91a37608fe8fcd0f2808c155ad7936a4f4e777a9
basar batur <baturbasar@gmail.com>
2022-08-25 18:18:38 -0500

    fixing test4

--- src/TestDemo.java
+++ src/TestDemo.java
@@ -16,7 +16,7 @@ public class TestDemo {
 
 	@Test
 	public void test4() {
-		System.out.println("Test 3");
-		System.out.println("Test 3");
+		System.out.println("Test 4");
+		System.out.println("Test 4");
 	}
 }
