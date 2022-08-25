bc452cd30446380c13d3e1974965cc40f2de4b6d
basarbatur <48811461+basarbatur@users.noreply.github.com>
2022-08-25 18:20:03 -0500

    [KAL-52] Adding test 4 (#2)

--- src/TestDemo.java
+++ src/TestDemo.java
@@ -7,12 +7,16 @@ public class TestDemo {
 
 	@Test
 	public void test1() {
-		System.out.println("Test 1");
+		System.out.println("Test");
 	}
 	
 	@Test
 	public void test3() {
-		System.out.println("Test 3");
-		System.out.println("Test 3");
+	}
+
+	@Test
+	public void test4() {
+		System.out.println("Test 4");
+		System.out.println("Test 4");
 	}
 }
